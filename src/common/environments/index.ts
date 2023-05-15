7a05293437ca55bf70809169dee64818a4135c40
hieudoanm <hieumdoan@gmail.com>
2023-05-15 17:56:05 +0700

    Add dotenv

--- src/common/environments/index.ts
+++ src/common/environments/index.ts
@@ -1,2 +1,8 @@
+import dotenv from 'dotenv';
+import dotenvExpand from 'dotenv-expand';
+
+const environments = dotenv.config();
+dotenvExpand.expand(environments);
+
 export const JWT_SECRET: string = process.env.JWT_SECRET || '';
 export const SALT_OR_ROUNDS: string = process.env.SALT_OR_ROUNDS || '10';
